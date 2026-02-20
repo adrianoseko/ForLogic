ab0d4e19c1cd4193de0b61fa66b9ff58ec4c88b6
Adriano Jorge de Moraes <68881626+adrianoseko@users.noreply.github.com>
2026-02-20 16:51:43 -0300

    refactor: frontend/src/app/app-template/pages/login/login.component.spec.ts via HighDev.AI

--- frontend/src/app/app-template/pages/login/login.component.spec.ts
+++ frontend/src/app/app-template/pages/login/login.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { LoginComponent } from './login.component';
 
 describe('LoginComponent', () => {
@@ -8,9 +7,8 @@ describe('LoginComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
-    })
-    .compileComponents();
+      declarations: [LoginComponent],
+    }).compileComponents();
   });
 
   beforeEach(() => {
@@ -19,7 +17,7 @@ describe('LoginComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create the LoginComponent', () => {
     expect(component).toBeTruthy();
   });
 });
