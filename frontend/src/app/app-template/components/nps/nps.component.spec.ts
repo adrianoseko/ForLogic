bf808c144f5d1193e60e152872834f192d7b2755
Adriano Jorge de Moraes <68881626+adrianoseko@users.noreply.github.com>
2026-02-20 16:51:23 -0300

    refactor: frontend/src/app/app-template/components/nps/nps.component.spec.ts via HighDev.AI

--- frontend/src/app/app-template/components/nps/nps.component.spec.ts
+++ frontend/src/app/app-template/components/nps/nps.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { NpsComponent } from './nps.component';
 
 describe('NpsComponent', () => {
@@ -8,9 +7,8 @@ describe('NpsComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ NpsComponent ]
-    })
-    .compileComponents();
+      declarations: [NpsComponent],
+    }).compileComponents();
   });
 
   beforeEach(() => {
@@ -19,7 +17,7 @@ describe('NpsComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create the NpsComponent', () => {
     expect(component).toBeTruthy();
   });
 });
