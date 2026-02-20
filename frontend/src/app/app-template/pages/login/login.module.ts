e797b7ef346980f9a73b8d54247f15e730361ee5
Adriano Jorge de Moraes <68881626+adrianoseko@users.noreply.github.com>
2026-02-20 16:51:45 -0300

    refactor: frontend/src/app/app-template/pages/login/login.module.ts via HighDev.AI

--- frontend/src/app/app-template/pages/login/login.module.ts
+++ frontend/src/app/app-template/pages/login/login.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-
-
+/**
+ * Module for the Login feature.
+ */
 @NgModule({
   declarations: [],
   imports: [
     CommonModule
-  ]
+  ],
+  exports: []
 })
 export class LoginModule { }
