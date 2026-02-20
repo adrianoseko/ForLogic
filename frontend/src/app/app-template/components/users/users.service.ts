7586ca613c960123a2218819d60b4bb55fc9c44f
Adriano Jorge de Moraes <68881626+adrianoseko@users.noreply.github.com>
2026-02-20 16:51:36 -0300

    refactor: frontend/src/app/app-template/components/users/users.service.ts via HighDev.AI

--- frontend/src/app/app-template/components/users/users.service.ts
+++ frontend/src/app/app-template/components/users/users.service.ts
@@ -1,23 +1,29 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs/internal/Observable';
+import { Observable } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { environment as env } from '../../../../environments/environment';
 import { environment } from '../../../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
-  private readonly PATH: string = '';
+  private readonly apiUrl: string = `${environment.baseApiUrl}/api/Users`;
 
   constructor(private http: HttpClient) { }
 
-  getUsers(): Observable<any> {
-    const header = new HttpHeaders().set(
-      'Content-Type',
-      'application/json; charset=utf-8'
-    );
-    console.log(env.baseApiUrl);
-    return this.http.get<any>('https://localhost:7136/api/Users')
+  getUsers(): Observable<User[]> {
+    const headers = this.createHeaders();
+    return this.http.get<User[]>(this.apiUrl, { headers });
   }
+
+  private createHeaders(): HttpHeaders {
+    return new HttpHeaders({ 'Content-Type': 'application/json; charset=utf-8' });
+  }
+}
+
+export interface User {
+  id: number;
+  name: string;
+  email: string;
+  // Add other user properties as needed
 }
