f2e75f87384f5dffeea356c7b97c9c04f06e893b
Adriano Jorge de Moraes <68881626+adrianoseko@users.noreply.github.com>
2026-02-20 16:51:08 -0300

    refactor: backend/Model/Users.cs via HighDev.AI

--- backend/Model/Users.cs
+++ backend/Model/Users.cs
@@ -1,10 +1,34 @@
-namespace users.Model
+namespace Users.Model
 {
-    public class Users
+    public class User
     {
-        public int Id { get; set; }
-        public string Login { get; set; }
-        public string Senha { get; set; }
+        public int Id { get; private set; }
+        public string Login { get; private set; }
+        public string Password { get; private set; }
 
+        public User(int id, string login, string password)
+        {
+            Id = id;
+            Login = login;
+            Password = password;
+        }
+
+        public void UpdateLogin(string newLogin)
+        {
+            if (string.IsNullOrWhiteSpace(newLogin))
+            {
+                throw new ArgumentException("Login cannot be empty or whitespace.", nameof(newLogin));
+            }
+            Login = newLogin;
+        }
+
+        public void UpdatePassword(string newPassword)
+        {
+            if (string.IsNullOrWhiteSpace(newPassword))
+            {
+                throw new ArgumentException("Password cannot be empty or whitespace.", nameof(newPassword));
+            }
+            Password = newPassword;
+        }
     }
 }
