f2638fef268df7a4a160a77d442e1900379f1bc1
Adriano Jorge de Moraes <68881626+adrianoseko@users.noreply.github.com>
2026-02-20 16:51:34 -0300

    refactor: frontend/src/app/app-template/components/users/users.component.ts via HighDev.AI

--- frontend/src/app/app-template/components/users/users.component.ts
+++ frontend/src/app/app-template/components/users/users.component.ts
@@ -1,23 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from './users.service';
+import { User } from './user.model';
+
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.scss']
 })
 export class UsersComponent implements OnInit {
-  users: any;
+  users: User[] = [];
 
-  constructor(private userService: UserService,) { }
+  constructor(private userService: UserService) { }
 
   ngOnInit(): void {
-    this.getClient()
+    this.loadUsers();
   }
 
-  getClient() {
-    this.userService.getUsers().subscribe(
-      data => { console.log(data), this.users = data }
-    )
+  private loadUsers(): void {
+    this.userService.getUsers().subscribe({
+      next: (data: User[]) => this.handleUserData(data),
+      error: (error) => this.handleError(error)
+    });
   }
 
+  private handleUserData(data: User[]): void {
+    this.users = data;
+    console.log(this.users);
+  }
+
+  private handleError(error: any): void {
+    console.error('Error fetching users:', error);
+  }
 }
